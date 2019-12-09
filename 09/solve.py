737131e9d479a37cf296d728d50a620796fcfec9
tony chang <tony@ponderer.org>
2019-12-09 09:44:29 -0800

    Refactor IntCode interpreter.
    Cleaned up the code by moving logging into
    a method and out address computation into a method.

--- 09/solve.py
+++ 09/solve.py
@@ -1,13 +1,12 @@
 mem_orig = map(int, open('input').read().split(','))
 
-class Amplifier(object):
-  def __init__(self, phase, mem):
-    self.phase = phase
+class IntCode(object):
+  def __init__(self, mem, id=None):
+    self.id = id
     self.ip = 0
     self.relative_base = 0
     # guess the size for now
     self.mem = mem[:] + [0] * 1000
-    self.run(phase)
 
   def get_opcode_and_modes(self):
     n = self.mem[self.ip]
@@ -27,79 +26,82 @@ class Amplifier(object):
         p = self.mem[self.ip+offset]
       elif mode == 2:
         p = self.mem[self.relative_base + self.mem[self.ip+offset]]
-        print 'relative base', self.relative_base, 'param', self.mem[self.ip+offset], 'value', p
+        #self.log(' relative base {}, param {}, value {}'.format(self.relative_base, self.mem[self.ip+offset], p))
       else:
         raise Exception('Unknown mode: {}'.format(modes[offset-1]))
       params.append(p)
     return params
 
+  def get_out_addr(self, param, modes):
+    addr = self.mem[self.ip+param]
+    if modes[param-1] == 2:
+      addr += self.relative_base
+    elif modes[param-1] != 0:
+      raise Exception('Unknown mode for out addr: {}'.format(modes[param-1]))
+    return addr
+
+  def log(self, msg):
+    out = ''
+    if self.id:
+      out += '[{}]'.format(self.id)
+    print('{} {}'.format(out, msg))
+
   def run(self, inp):
     while True:
       opcode, modes = self.get_opcode_and_modes()
       if opcode == 99:
-        print '[Phase {}] Exiting'.format(self.phase)
-        raise StopIteration()
+        self.log('Exiting')
+        return
 
-      print self.mem[self.ip], opcode, modes
+      #self.log('{} -> {} {}'.format(self.mem[self.ip], opcode, modes))
 
       if opcode == 1:
         p1, p2 = self.read_params(2, modes)
-        print 'mem[{}] = {} + {}'.format(self.mem[self.ip+3], p1, p2)
-        addr = self.mem[self.ip+3]
-        if modes[2] == 2:
-          addr += self.relative_base
+        addr = self.get_out_addr(3, modes)
+        #self.log(' mem[{}] = {} + {}'.format(addr, p1, p2))
         self.mem[addr] = p1 + p2
         self.ip += 4
       elif opcode == 2:
         p1, p2 = self.read_params(2, modes)
-        print 'mem[{}] = {} * {}'.format(self.mem[self.ip+3], p1, p2)
-        addr = self.mem[self.ip+3]
-        if modes[2] == 2:
-          addr += self.relative_base
+        addr = self.get_out_addr(3, modes)
+        #self.log(' mem[{}] = {} * {}'.format(addr, p1, p2))
         self.mem[addr] = p1 * p2
         self.ip += 4
       elif opcode == 3:
         if inp is None:
-          print '[Phase {}] Waiting for input'.format(self.phase)
+          self.log('Waiting for input')
           return
-        addr = self.mem[self.ip+1]
-        if modes[0] == 2:
-          addr += self.relative_base
+        addr = self.get_out_addr(1, modes)
         self.mem[addr] = inp
         inp = None
         self.ip += 2
       elif opcode == 4:
         p1, = self.read_params(1, modes)
-        print '[Phase {}] Output:'.format(self.phase), p1
+        self.log('Output: {}'.format(p1))
         output = p1
         self.ip += 2
-        #return output
       elif opcode == 5:
         p1, p2 = self.read_params(2, modes)
-        print 'ip = {} != 0 ? {} : {}'.format(p1, p2, self.ip+3)
+        #self.log(' ip = {} != 0 ? {} : {}'.format(p1, p2, self.ip+3))
         self.ip = p2 if p1 != 0 else self.ip+3
       elif opcode == 6:
         p1, p2 = self.read_params(2, modes)
         self.ip = p2 if p1 == 0 else self.ip+3
       elif opcode == 7:
         p1, p2 = self.read_params(2, modes)
-        addr = self.mem[self.ip+3]
-        if modes[2] == 2:
-          addr += self.relative_base
-        print 'mem[{}] = {} < {}'.format(addr, p1, p2)
+        addr = self.get_out_addr(3, modes)
+        #self.log(' mem[{}] = {} < {}'.format(addr, p1, p2))
         self.mem[addr] = 1 if p1 < p2 else 0
         self.ip += 4
       elif opcode == 8:
         p1, p2 = self.read_params(2, modes)
-        addr = self.mem[self.ip+3]
-        if modes[2] == 2:
-          addr += self.relative_base
-        print 'mem[{}] = {} == {}'.format(addr, p1, p2)
+        addr = self.get_out_addr(3, modes)
+        #self.log(' mem[{}] = {} == {}'.format(addr, p1, p2))
         self.mem[addr] = 1 if p1 == p2 else 0
         self.ip += 4
       elif opcode == 9:
         p1, = self.read_params(1, modes)
-        print 'adjust relative base by', p1, 'to', self.relative_base + p1
+        #self.log(' adjust relative base by {} to {}'.format(p1, self.relative_base + p1))
         self.relative_base += p1
         self.ip += 2
       else:
@@ -108,15 +110,10 @@ class Amplifier(object):
 #mem_orig = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
 #mem_orig = [1102,34915192,34915192,7,4,7,99,0]
 #mem_orig = [104,1125899906842624,99]
-try:
-  print 'part1'
-  a = Amplifier(1, mem_orig)
-except StopIteration:
-  pass
+print 'part1'
+ic = IntCode(mem_orig)
+ic.run(1)
 
-try:
-  print 'part2'
-  a = Amplifier(2, mem_orig)
-except StopIteration:
-  pass
-
+print 'part2'
+ic = IntCode(mem_orig)
+ic.run(2)
